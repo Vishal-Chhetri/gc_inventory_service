58d293e126e30586d41eb9d5b61430af6aa63c2c
Vishal-Chhetri <42584441+Vishal-Chhetri@users.noreply.github.com>
2018-10-17 00:19:21 +0530

    Initial commit

---
+++ src/main/java/com/computer/genuine/model/Vendor.java
@@ -0,0 +1,81 @@
+package com.computer.genuine.model;
+
+import java.util.Date;
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+/**
+ * @author Vishal Chhetri
+ */
+@Entity
+@Table(name="VENDOR")
+public class Vendor {
+@Id
+@GeneratedValue(strategy=GenerationType.IDENTITY)
+@Column(name="USER_ID")
+private Long user_id;
+@Column(name="NAME")
+private String name;
+@Column(name="CREATED_DATE")
+private Date createdDate;
+@Column(name="LAST_UPDATED_DATE")
+private Date lastUpdDate;
+
+@OneToMany(targetEntity=VendorDetails.class,cascade=CascadeType.ALL)
+@JoinColumn(name="USER_ID")
+private List<VendorDetails> vendorDetailsList;
+
+public Vendor() {
+	super();
+}
+
+public Long getUser_id() {
+	return user_id;
+}
+
+public void setUser_id(Long user_id) {
+	this.user_id = user_id;
+}
+
+public String getName() {
+	return name;
+}
+
+public void setName(String name) {
+	this.name = name;
+}
+
+public Date getCreatedDate() {
+	return createdDate;
+}
+
+public void setCreatedDate(Date createdDate) {
+	this.createdDate = createdDate;
+}
+
+public Date getLastUpdDate() {
+	return lastUpdDate;
+}
+
+public void setLastUpdDate(Date lastUpdDate) {
+	this.lastUpdDate = lastUpdDate;
+}
+
+public List<VendorDetails> getVendorDetailsList() {
+	return vendorDetailsList;
+}
+
+public void setVendorDetailsList(List<VendorDetails> vendorDetailsList) {
+	this.vendorDetailsList = vendorDetailsList;
+}
+	
+}
---
+++ src/main/java/com/computer/genuine/model/VendorDetails.java
@@ -0,0 +1,75 @@
+package com.computer.genuine.model;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+/**
+ * @author Vishal Chhetri
+ *
+ */
+@Entity
+@Table(name="VENDOR_DETAILS")
+public class VendorDetails {
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Column(name="USER_ID")
+	private long user_id;
+	@Column(name="ADDRESS")
+	private String address;
+	@Column(name="PRIMARY_CONTACT_NO")
+	private String primaryContactNo;
+	@Column(name="SECONDARY_CONTACT_NO")
+	private String secondaryContactNo;
+	@Column(name = "CREATED_DATE")
+	private Date createdDate;
+	@Column(name = "LAST_UPDATED_DATE")
+	private Date lastUpdatedDate;
+	public VendorDetails() {
+		super();
+	}
+	public long getUser_id() {
+		return user_id;
+	}
+	public void setUser_id(long user_id) {
+		this.user_id = user_id;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getPrimaryContactNo() {
+		return primaryContactNo;
+	}
+	public void setPrimaryContactNo(String primaryContactNo) {
+		this.primaryContactNo = primaryContactNo;
+	}
+	public String getSecondaryContactNo() {
+		return secondaryContactNo;
+	}
+	public void setSecondaryContactNo(String secondaryContactNo) {
+		this.secondaryContactNo = secondaryContactNo;
+	}
+	public Date getCreatedDate() {
+		return createdDate;
+	}
+	public void setCreatedDate(Date createdDate) {
+		this.createdDate = createdDate;
+	}
+	public Date getLastUpdatedDate() {
+		return lastUpdatedDate;
+	}
+	public void setLastUpdatedDate(Date lastUpdatedDate) {
+		this.lastUpdatedDate = lastUpdatedDate;
+	}
+	
+	
+	
+}
